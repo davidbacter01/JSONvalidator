9ed2cf5116c73b6e2e536933a260a3b169d69056
davidbacter01 <davidbacter01@gmail.com>
2020-02-25 12:59:06 +0200

    refactored code to work for all float numbers

--- JsonNumberValidator/JsonNumberValidator/Program.cs
+++ JsonNumberValidator/JsonNumberValidator/Program.cs
@@ -4,6 +4,7 @@ namespace JsonNumberValidator
 {
     public class Program
     {
+        public const string DecimalSistem = "0123456789";
         public static void Main()
         {
             string numberToCheck = Console.ReadLine();
@@ -12,22 +13,21 @@ namespace JsonNumberValidator
 
         public static string GetJSONNumberValidity(string number)
         {
-            if (number == "12.34")
-            {
-                return "Valid";
-            }
-
-            const string DecimalSistem = "0123456789";
-            if (!DecimalSistem.Contains(number[0]) && !(number[0] == '-'))
+            if (number[0] != '-' && !DecimalSistem.Contains(number[0]))
             {
                 return "Invalid";
             }
 
+            bool hasFloatingPoint = false;
             for (int i = 1; i < number.Length; i++)
             {
-                if (!DecimalSistem.Contains(number[i]))
+                if (number[i] == '.' && hasFloatingPoint)
                 {
                     return "Invalid";
+                }
+                else if (number[i] == '.' && !hasFloatingPoint)
+                {
+                    hasFloatingPoint = true;
                 }
             }
 
