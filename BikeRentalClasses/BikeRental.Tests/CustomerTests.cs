6144b20e28d8f072c116fdaab3440d36d5834acb
davidbacter01 <davidbacter01@gmail.com>
2020-03-02 22:05:32 +0200

    added new test case goes straight to green

--- BikeRentalClasses/BikeRental.Tests/CustomerTests.cs
+++ BikeRentalClasses/BikeRental.Tests/CustomerTests.cs
@@ -27,5 +27,12 @@ namespace BikeRentalClasses.Tests
             Customer nelu = new Customer(5, 1, 0, 1);
             Assert.Equal(115.00, nelu.TotalAmount());
         }
+
+        [Fact]
+        public void CustomerThatRentsOneBikeEachForAweekShouldReturnCorrectTotalAmount()
+        {
+            Customer nelu = new Customer(7, 1, 1, 1);
+            Assert.Equal(182.00, nelu.TotalAmount());
+        }
     }
 }
