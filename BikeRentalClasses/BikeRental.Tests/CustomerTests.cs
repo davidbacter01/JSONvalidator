a6018715c66d01e3868ac5292892b8a61272a63a
davidbacter01 <davidbacter01@gmail.com>
2020-03-02 21:33:47 +0200

    added new test green

---
+++ BikeRentalClasses/BikeRental.Tests/CustomerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BikeRentalClasses.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void CustomerThatRentsOneBikeForADayShouldReturnCorrectTotalAmount()
+        {
+            Customer nelu = new Customer(1);
+            Assert.Equal(17.00, nelu.TotalAmount());
+        }
+    }
+}
---
+++ BikeRentalClasses/BikeRentalClasses/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeRentalClasses
+{
+    public class Customer
+    {
+        private readonly int rentingDays;
+        private readonly Bike sport;
+        public Customer(int rentingDays)
+        {
+            this.rentingDays = rentingDays;
+            this.sport = new Bike("Sport");
+        }
+
+        public double TotalAmount()
+        {
+            return sport.GetPriceForDays(rentingDays);
+        }
+    }
+}
