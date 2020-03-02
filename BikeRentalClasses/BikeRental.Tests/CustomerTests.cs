26aeed735b95dc4df51ba240a8d155f64ae3de9c
davidbacter01 <davidbacter01@gmail.com>
2020-03-02 22:02:00 +0200

    refactored code and added new test to green

--- BikeRentalClasses/BikeRental.Tests/CustomerTests.cs
+++ BikeRentalClasses/BikeRental.Tests/CustomerTests.cs
@@ -10,15 +10,22 @@ namespace BikeRentalClasses.Tests
         [Fact]
         public void CustomerThatRentsOneSportBikeForADayShouldReturnCorrectTotalAmount()
         {
-            Customer nelu = new Customer(1, 1, 0);
+            Customer nelu = new Customer(1, 1, 0, 0);
             Assert.Equal(17.00, nelu.TotalAmount());
         }
 
         [Fact]
         public void CustomerThatRentsOneCityBikeForMoreDaysShouldReturnCorrectTotalAmount()
         {
-            Customer nelu = new Customer(5, 0, 1);
+            Customer nelu = new Customer(5, 0, 1, 0);
             Assert.Equal(35.00, nelu.TotalAmount());
+        }
+
+        [Fact]
+        public void CustomerThatRentsOneJuniorBikeAndOneSportForMoreDaysShouldReturnCorrectTotalAmount()
+        {
+            Customer nelu = new Customer(5, 1, 0, 1);
+            Assert.Equal(115.00, nelu.TotalAmount());
         }
     }
 }
--- BikeRentalClasses/BikeRentalClasses/Customer.cs
+++ BikeRentalClasses/BikeRentalClasses/Customer.cs
@@ -9,20 +9,26 @@ namespace BikeRentalClasses
         private readonly int rentingDays;
         private readonly int sportBikes;
         private readonly int cityBikes;
+        private readonly int juniorBikes;
         private readonly Bike sport;
         private readonly Bike city;
-        public Customer(int rentingDays, int sportBikes, int cityBikes)
+        private readonly Bike junior;
+        public Customer(int rentingDays, int sportBikes, int cityBikes, int juniorBikes)
         {
             this.rentingDays = rentingDays;
             this.sportBikes = sportBikes;
             this.cityBikes = cityBikes;
+            this.juniorBikes = juniorBikes;
             this.sport = new Bike("Sport");
             this.city = new Bike("City");
+            this.junior = new Bike("Junior");
         }
 
         public double TotalAmount()
         {
-            return sport.GetPriceForDays(rentingDays)*sportBikes+city.GetPriceForDays(rentingDays)*cityBikes;
+            return sport.GetPriceForDays(rentingDays) * sportBikes +
+                city.GetPriceForDays(rentingDays) * cityBikes +
+                junior.GetPriceForDays(rentingDays) * juniorBikes;
         }
     }
 }
