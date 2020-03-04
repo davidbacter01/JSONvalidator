d5f5664cad19a2d95a35f1dcb0dcc5760f677e21
davidbacter01 <davidbacter01@gmail.com>
2020-03-04 10:54:16 +0200

    added class Character ande
    tests to green

---
+++ JSONClasses/RangeClass.Tests/CharacterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class CharacterTests
+    {
+        [Fact]
+        public void ValidCharacterMatchShouldReturnTrue()
+        {
+            Character x = new Character('x');
+            bool expected = true;
+            Assert.Equal(expected, x.Match("xilo"));
+        }
+
+        [Fact]
+        public void InvalidCharacterMatchShouldReturnFalse()
+        {
+            Character x = new Character('x');
+            bool expected = false;
+            Assert.Equal(expected, x.Match("filo"));
+        }
+
+        [Fact]
+        public void CharacterMatchWithNullShouldReturnFalse()
+        {
+            Character x = new Character('x');
+            bool expected = false;
+            Assert.Equal(expected, x.Match(null));
+        }
+    }
+}
