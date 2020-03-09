dc11677bca29f9d8da4f8aba9679611ce3df61c7
davidbacter01 <davidbacter01@gmail.com>
2020-03-09 12:51:33 +0200

    added Text class and tests passing to green

---
+++ JSONClasses/RangeClass.Tests/TextTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class TextTests
+    {
+        private static IPattern tru = new Text("true");
+
+        [Fact]
+        public void EntireTextMatchingPrefixShouldReturnEmptyText()
+        {
+            Assert.Equal("", tru.Match("true").RemainingText());
+        }
+
+        [Fact]
+        public void TextMatchingPrefixShouldReturnRemainingText()
+        {
+            Assert.Equal("X", tru.Match("trueX").RemainingText());
+        }
+
+        [Fact]
+        public void TextNotMatchingPrefixShouldReturnInitialText()
+        {
+            Assert.Equal("false", tru.Match("false").RemainingText());
+        }
+
+        [Fact]
+        public void EmptyTextShouldReturnEmptyText()
+        {
+            Assert.Equal("", tru.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void NullShouldReturnNull()
+        {
+            Assert.Null(tru.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void EmptyPrefixMatchingTextShouldReturnInitialText()
+        {
+            var empty = new Text("");
+            Assert.Equal("true", empty.Match("true").RemainingText());
+        }
+    }
+}
---
+++ JSONClasses/RangeClass/Text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Text : IPattern
+    {
+        private readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return new FailMatch(text);
+
+
+            return text.StartsWith(prefix)
+                ? new SuccesMatch(text.Remove(0, prefix.Length)) as IMatch
+                : new FailMatch(text);
+        }
+    }
+}
