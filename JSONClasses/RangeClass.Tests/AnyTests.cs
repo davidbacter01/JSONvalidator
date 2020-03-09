877dbc94e754ddc3723f8499222f28d2af4ac55e
davidbacter01 <davidbacter01@gmail.com>
2020-03-09 12:22:02 +0200

    Added class Any and tests passing all to green

---
+++ JSONClasses/RangeClass.Tests/AnyTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class AnyTests
+    {
+        private static IPattern e = new Any("eE");
+        [Fact]
+        public void MatchingFirstLetterShouldReturnRemainingText()
+        {
+            Assert.Equal("a", e.Match("ea").RemainingText());
+        }
+
+        [Fact]
+        public void MatchingFirstLetterInOtherPartOfConstructorTextShouldReturnRemainingText()
+        {
+            Assert.Equal("a", e.Match("Ea").RemainingText());
+        }
+
+        [Fact]
+        public void NotMatchingFirstLetterShouldReturnInitialText()
+        {
+            Assert.Equal("a", e.Match("a").RemainingText());
+        }
+
+        [Fact]
+        public void EmptyTextShouldReturnEmptyText()
+        {
+            Assert.Equal("", e.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void NullTextShouldReturnNull()
+        {
+            Assert.Null(e.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void MatchingSignShouldReturnRemainingText()
+        {
+            var sign = new Any("+-");
+            Assert.Equal("3", sign.Match("-3").RemainingText());
+        }
+    }
+}
---
+++ JSONClasses/RangeClass/Any.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Any : IPattern
+    {
+        private readonly string accepted;
+        public Any(string accepted) 
+        {
+            this.accepted = accepted;
+        }
+            
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return new FailMatch(text);
+
+            return accepted.Contains(text[0])
+                ? new SuccesMatch(text[1..^0]) as IMatch
+                : new FailMatch(text);
+        }
+    }
+}
