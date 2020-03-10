b8dfacdd621d861ef1826112a9405fdb5397964b
davidbacter01 <davidbacter01@gmail.com>
2020-03-10 16:12:57 +0200

    Added class Many and tests passing to green

---
+++ JSONClasses/RangeClass.Tests/ManyTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class ManyTests
+    {
+        [Fact]
+        public void MatchingCharAtStartOfTextShouldReturnRemainingText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void MatchingMultipleCopiesOfCharAtStartOfTextShouldReturnRemainingText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.Equal("bc", a.Match("aaaaabc").RemainingText());
+        }
+
+        [Fact]
+        public void NotMatchingCharAtStartOfTextShouldReturnInitialText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void EmptyTextShouldReturnEmptyText()
+        {
+            var a = new Many(new Character('a'));
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void NullShouldReturnNull()
+        {
+            var a = new Many(new Character('a'));
+            Assert.Null(a.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void DigitsRangeAtStartShouldConsumeAllDigitsInRange()
+        {
+            var digits = new Many(new Range('0', '9'));
+            Assert.Equal("ab123", digits.Match("12345ab123").RemainingText());
+        }
+
+        [Fact]
+        public void DigitsRangeAtStartNotMatchinShouldReturnInitialText()
+        {
+            var digits = new Many(new Range('0', '9'));
+            Assert.Equal("ab", digits.Match("ab").RemainingText());
+        }
+    }
+}
---
+++ JSONClasses/RangeClass/Many.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Many : IPattern
+    {
+        private readonly IPattern pattern;
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            string consumedText = pattern.Match(text).RemainingText();
+            while (pattern.Match(consumedText).Success())
+            {
+                consumedText = pattern.Match(consumedText).RemainingText();
+            }
+
+            return new SuccesMatch(consumedText);          
+        }
+    }
+}
