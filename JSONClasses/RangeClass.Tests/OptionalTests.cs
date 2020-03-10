5687ec58fca227f75d85a13855200bc8f900e883
davidbacter01 <davidbacter01@gmail.com>
2020-03-10 18:54:41 +0200

    added Optional class and tests go green

---
+++ JSONClasses/RangeClass.Tests/OptionalTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class OptionalTests
+    {
+      /*  var a = new Optional(new Character('a'));
+        a.Match(null); // true / null
+        */
+
+        [Fact]
+        public void MatchingCharacterAtThextStartShouldReturnRestOfText()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void MatchingCharacterAtThextStartShouldConsumeOnlyOnce()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Equal("abc", a.Match("aabc").RemainingText());
+        }
+
+        [Fact]
+        public void NotMatchingCharacterAtThextStartShouldReturnInitialText()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void EmptyTextShouldReturnEmptyText()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void NullShouldReturnNull()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Null(a.Match(null).RemainingText());
+            Assert.True(a.Match(null).Success());
+        }
+    }
+}
---
+++ JSONClasses/RangeClass/Optional.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Optional : IPattern
+    {
+        private readonly IPattern pattern;
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return new SuccesMatch(pattern.Match(text).RemainingText());
+        }
+    }
+}
