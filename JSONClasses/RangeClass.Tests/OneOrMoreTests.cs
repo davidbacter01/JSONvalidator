7414fb40720c24d728dd0aa43f29bc4be2390057
davidbacter01 <davidbacter01@gmail.com>
2020-03-12 12:55:56 +0200

    added class OneOrMore and tests running on green

---
+++ JSONClasses/RangeClass.Tests/OneOrMoreTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class OneOrMoreTests
+    {
+        [Fact]
+        public void OnlyDigitsShouldReturnEmptyTextTrue()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.True(a.Match("123").Success());
+            Assert.Equal("", a.Match("123").RemainingText());
+        }
+
+        [Fact]
+        public void MatchingDigitsShouldReturnRemainingTextTrue()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.True(a.Match("1a").Success());
+            Assert.Equal("a", a.Match("1a").RemainingText());
+        }
+
+        [Fact]
+        public void NotMatchingDigitsShouldReturnInitialTextFalse()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match("bc").Success());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void EmptyTextShouldReturnEmptyTextFalse()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match("").Success());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void NullShouldReturnNullFalse()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match(null).Success());
+            Assert.Null( a.Match(null).RemainingText());
+        }
+    }
+}
---
+++ JSONClasses/RangeClass/OneOrMore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Sequence(pattern, new Many(pattern));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
