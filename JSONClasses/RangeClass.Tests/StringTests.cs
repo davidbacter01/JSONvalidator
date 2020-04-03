abaef05ffb16048625cbed82cf6617741507fb05
davidbacter01 <davidbacter01@gmail.com>
2020-04-03 20:11:33 +0300

    added class String and first red test

---
+++ JSONClasses/RangeClass.Tests/StringTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Classes.Tests
+{
+    public class StringTests
+    {
+        IPattern text = new String();
+
+        [Fact]
+        public void SimpleTextShouldReturnTrueAndEmptyString()
+        {
+            Assert.True(text.Match("asd").Success());
+            Assert.Equal("", text.Match("asd").RemainingText());
+        }
+    }
+}
---
+++ JSONClasses/RangeClass/String.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class String : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+            pattern =;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
