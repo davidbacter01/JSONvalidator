e869d7a29f13ad1f174742290c6f1c1d10537c30
davidbacter01 <davidbacter01@gmail.com>
2020-04-23 22:06:18 +0300

    made changes

--- JSONClasses/RangeClass.Tests/ValueTests.cs
+++ JSONClasses/RangeClass.Tests/ValueTests.cs
@@ -7,13 +7,13 @@ namespace Classes.Tests
 {
     public class ValueTests
     {
-        IPattern value = new Value();
+        readonly IPattern value = new Value();
 
         [Fact]
         public void ValidObjectShouldReturnTrueAndEmptyString()
         {
            // Assert.True(value.Match("[ \"\" ]").Success());
-            Assert.Equal("", value.Match("[ ]").RemainingText());
+            Assert.Equal("", value.Match("[ 123 ]").RemainingText());
         }
 
         [Fact]
