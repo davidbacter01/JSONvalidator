bb4290e1e8bb7c3013537540e2e93feb4b281df7
davidbacter01 <davidbacter01@gmail.com>
2020-04-25 16:26:10 +0300

    finished the console app, it works correct

---
+++ JSONClasses/JsonValidatorConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Classes;
+using System;
+namespace JsonValidatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = new Value();
+            int pathNumber = 1;
+            foreach(string path in args)
+            {
+                string text = System.IO.File.ReadAllText(args[0]);
+                Console.WriteLine("Path number {0} leads to a valid JSON value :" + (json.Match(text).RemainingText() == ""), pathNumber);
+                pathNumber++;
+            }
+        }
+    }
+}
