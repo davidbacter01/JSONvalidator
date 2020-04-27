439560e9d6fc79d9221f99df7d1e592fa5ec858e
davidbacter01 <davidbacter01@gmail.com>
2020-04-27 04:01:19 +0300

    extend functionality to find the error location

--- JSONClasses/JsonValidatorConsoleApp/Program.cs
+++ JSONClasses/JsonValidatorConsoleApp/Program.cs
@@ -6,14 +6,28 @@ namespace JsonValidatorConsoleApp
     {
         static void Main(string[] args)
         {
-            var json = new Value();
             int pathNumber = 1;
             foreach(string path in args)
             {
+                var json = new Value();
                 string text = System.IO.File.ReadAllText(args[0]);
                 Console.WriteLine("Path number {0} leads to a valid JSON value :" + (json.Match(text).RemainingText() == ""), pathNumber);
                 pathNumber++;
+
+                if (json.Match(text).RemainingText() != "")
+                    PrintErrorLocation(text, json);
             }
+
+            Console.Read();
+        }
+
+        private static void PrintErrorLocation(string text, IPattern json)
+        {
+            string[] textLines = text.Split('\n');
+            string remainingText = json.Match(text).RemainingText();
+            int errorLine = Array.IndexOf(textLines, remainingText) + 1;
+            int errorColumn = textLines[errorLine - 1].IndexOf(remainingText);
+            Console.WriteLine("Validation fails on line {0} at position {1}", errorLine, errorColumn);
         }
     }
 }
