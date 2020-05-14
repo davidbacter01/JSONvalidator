bbb5d2f33057d3aa10869822c2bc263b592ac0d3
davidbacter01 <davidbacter01@gmail.com>
2020-05-14 03:11:34 +0300

    completed ObjectArray

--- intArrayClasses/intArrayClasses/ObjectArray.cs
+++ intArrayClasses/intArrayClasses/ObjectArray.cs
@@ -1,8 +1,10 @@
-﻿namespace intArrayClasses
+﻿using System;
+
+namespace intArrayClasses
 {
     class ObjectArray
     {
-        object[] array;
+        private object[] array;
         private const int Size = 4;
         public ObjectArray()
         {
@@ -10,5 +12,81 @@
         }
 
         public int Count { get; private set; } = 0;
+
+        public virtual object this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public void Add(object element)
+        {
+            ResizeIfNeeded();
+            Count++;
+            array[Count - 1] = element;
+        }
+
+        public bool Contains(object element) => IndexOf(element) > -1;
+
+        public int IndexOf(object element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, object element)
+        {
+            ResizeIfNeeded();
+            ShiftRight(index);
+            array[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(array, 0, array.Length);
+            Count = 0;
+        }
+
+        public void Remove(object element) => RemoveAt(IndexOf(element));
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[Count - 1] = 0;
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = Count; i >= index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+        }
+
+        private void ResizeIfNeeded()
+        {
+            if (array.Length == Count)
+            {
+                Array.Resize(ref array, Count * 2);
+            }
+        }
     }
 }
