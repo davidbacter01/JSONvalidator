339562212e1dfa2a22607ba55c46cc04f17f8fd8
davidbacter01 <davidbacter01@gmail.com>
2020-08-15 16:28:45 +0300

    refactors GetWithNoFeatures

--- LinqExercices/Functions.cs
+++ LinqExercices/Functions.cs
@@ -142,11 +142,9 @@ namespace LinqExercices
                 throw new ArgumentNullException();
             }
 
-            return products.Where(product => product
-            .Features.Select(feat => feat.Id)
-            .Intersect(
-                features.Select(feat => feat.Id))
-            .Count() == 0);
+            return products.Where(p => !p.Features
+                .Select(f => f.Id)
+                .Any(id => features.Select(e => e.Id).Contains(id)));
         }
 
         public static IEnumerable<ProductS> MergeLists(IEnumerable<ProductS> first, IEnumerable<ProductS> second)
