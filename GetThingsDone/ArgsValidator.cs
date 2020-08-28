0c80b40203b303858ea93405f13b5e226c96db5c
davidbacter01 <davidbacter01@gmail.com>
2020-08-28 15:12:35 +0300

    refactors ArgsValidator and tests

--- GetThingsDone/ArgsValidator.cs
+++ GetThingsDone/ArgsValidator.cs
@@ -6,39 +6,112 @@ namespace GetThingsDone
     public class ArgsValidator
     {
         private readonly string[] _arguments;
+        private readonly string[] _simpleCommands;
         private readonly string[] _primaryCommands;
         private readonly string[] _secondaryCommands;
 
         public ArgsValidator(string[] args)
         {
             _arguments = args;
-            _primaryCommands = new[] {"add", "remove", "update", "list", "help"};
+            _simpleCommands = new[] {"list", "help"};
+            _primaryCommands = new[] {"add", "remove", "update"};
             _secondaryCommands = new[] {"--title", "--description", "--due_date", "--priority", "--complexity"};
         }
 
         public bool AreArgumentsValid()
         {
-            try
+            if (_simpleCommands.Contains(_arguments[0]))
             {
-                return _primaryCommands.Contains(_arguments[0]) && ValidateSecondaryArguments();
+                return _arguments.Length == 1;
             }
-            catch (Exception)
+
+            if (_primaryCommands.Contains(_arguments[0]))
+            {
+                return ValidatePrimaryCommands();
+            }
+
+            return false;
+        }
+
+        private bool ValidatePrimaryCommands()
+        {
+            return _arguments[0] switch
+            {
+                "add" => ValidateAdd(),
+                "remove" => ValidateRemove(),
+                "update" => ValidateUpdate(),
+                _ => false
+            };
+        }
+
+        private bool ValidateUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool ValidateRemove()
+        {
+            return _arguments[1] == "--title" &&
+                   _arguments[2] != null &&
+                   _arguments.Length == 3;
+        }
+
+        private bool ValidateAdd()
+        {
+            if (_arguments.Length < 3)
             {
                 return false;
             }
+
+            if (!ArgumentsHaveParamethers())
+            {
+                return false;
+            }
+
+            if (!_secondaryCommands.Contains(_arguments[1]))
+            {
+                return false;
+            }
+
+            return HasTitle() && ValidateSecondaryArguments();
+        }
+
+        private bool HasTitle()
+        {
+            for (int i = 1; i < _arguments.Length; i += 2)
+            {
+                if (_arguments[i] == "--title")
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private bool ValidateSecondaryArguments()
         {
-            if (_arguments.Length == 1 && _arguments[0] == "help")
-                return true;
-
-            if (_arguments.Length != 2 || _arguments.Length % 2 != 0)
-                return false;
-
-            for (int i = 2; i < _arguments.Length; i += 2)
+            for (int i = 1; i < _arguments.Length; i += 2)
             {
                 if (!_secondaryCommands.Contains(_arguments[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool ArgumentsHaveParamethers()
+        {
+            if (_arguments.Length % 2 != 1)
+            {
+                return false;
+            }
+
+            for (int i = 3; i < _arguments.Length; i++)
+            {
+                if (string.IsNullOrEmpty(_arguments[i]))
                 {
                     return false;
                 }
